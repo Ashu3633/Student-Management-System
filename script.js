531476e7050acdff5fca11d557c9a9bdf661de56
Ashu3633 <ashu12022002@gmail.com>
2026-06-11 20:22:31 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+function addStudent() {
+
+    let name = document.getElementById("studentName").value;
+
+    if(name === "") return;
+
+    let li = document.createElement("li");
+
+    li.innerHTML =
+        name +
+        " <button onclick='this.parentElement.remove()'>Delete</button>";
+
+    document.getElementById("studentList").appendChild(li);
+
+    document.getElementById("studentName").value = "";
+}
